9597b3ed6b0716649a980d15c7064d034511df41
Varenyam Sharma <varenyamsharma12@gmail.com>
2025-08-16 19:49:14 +0530

    Refactor code structure for improved readability and maintainability

---
+++ app/(landing page)/_components/heading.tsx
@@ -0,0 +1,23 @@
+"use client"
+import { Button } from '@/components/ui/button'
+import { ArrowRight } from 'lucide-react'
+import React from 'react'
+
+const Heading = () => {
+  return (
+    <div className='max-w-3xl space-y-4'>
+        <h1 className='text-3xl sm:text-5xl md:text-6xl font-bold'>
+            Simplify forms. Unify your workflow. Welcome to <span className='underline'>Notion Form</span>.
+        </h1>
+        <h3 className='text-base sm:text-xl md:text-2xl font-medium'>
+            Streamline your data collection and management with<br /> our powerful form solutions.
+        </h3>
+        <Button>
+            Enter Notion Form
+            <ArrowRight className='h-4 w-4 ml-2'/>
+        </Button>
+    </div>
+  )
+}
+
+export default Heading  
---
+++ app/(landing page)/_components/heroes.tsx
@@ -0,0 +1,24 @@
+import Image from 'next/image'
+import React from 'react'
+
+const Heroes = () => {
+  return (
+    <div className='flex flex-col items-center justify-center max-w-5xl'>
+        <div className='flex items-center'>
+            <div className="relative w-[300px] h-[300px] sm:w-[350px] sm:h-[350px] md:w-[400px] md:h-[400px]">
+                <Image
+                    src="/documents.png"
+                    fill
+                    className='object-contain'
+                    alt='documents'
+                />
+            </div>
+            <div>
+                
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Heroes
